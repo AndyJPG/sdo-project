45dda94a1f9c9b11582fb5f36ea9ea765aeb9920
peigengjiang <peigeng.jiang@preezie.com>
2022-04-26 20:52:52 +1000

    completed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,50 @@
 import MenuIcon from "@mui/icons-material/Menu"
 import SendIcon from "@mui/icons-material/Send"
-import { AppBar, Box, Button, IconButton, Toolbar, useScrollTrigger } from "@mui/material"
+import {
+  AppBar,
+  Box,
+  Button, Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Toolbar,
+  useScrollTrigger
+} from "@mui/material"
+import { useState } from "react"
 import logoOnly from "../assets/logo/svg-logo-only.svg"
 import logo from "../assets/logo/svg-logo.svg"
 
 const Navbar = () => {
+  const [ open, setOpen ] = useState(false)
   const trigger = useScrollTrigger({
     disableHysteresis: true,
-    threshold: 100,
+    threshold: 50,
     target: window
   })
+  const navPages = [ "about us", "our services", "case studies", "contact us" ]
 
   return (
     <>
+      <Drawer anchor="left" open={open} onBackdropClick={() => setOpen(false)}>
+        <Box sx={{ minWidth: 200, padding: "1rem" }}>
+          <List>
+            {navPages.map(page => (
+              <ListItem button key={page}>
+                <ListItemText primary={page}/>
+              </ListItem>
+            ))}
+          </List>
+          <Divider/>
+          <Button variant="contained" size="small" sx={{ mt: "1.6rem" }} endIcon={<SendIcon/>}>get in touch</Button>
+        </Box>
+      </Drawer>
       <Box flexGrow={1} sx={{ display: { xs: "block", md: "none" } }}>
-        <AppBar position="static" color="inherit" sx={{ boxShadow: "none" }}>
+        <AppBar position="fixed" color="inherit"
+                sx={{ boxShadow: `${trigger ? "rgba(149, 157, 165, 0.2) 0px 8px 24px" : "none"}` }}>
           <Toolbar>
-            <IconButton size="large" edge="start"><MenuIcon
+            <IconButton size="large" edge="start" onClick={() => setOpen(true)}><MenuIcon
               color="primary"/></IconButton>
             <Box
               sx={{
@@ -34,7 +62,7 @@ const Navbar = () => {
       </Box>
       <Box flexGrow={1} sx={{ display: { xs: "none", md: "block" } }}>
         <AppBar position="fixed" color={trigger ? "inherit" : "transparent"}
-                sx={{ boxShadow: `${trigger ? "auto" : "none"}` }}>
+                sx={{ boxShadow: `${trigger ? "rgba(149, 157, 165, 0.2) 0px 8px 24px" : "none"}` }}>
           <Toolbar>
             <Box sx={{
               display: "flex",
@@ -45,7 +73,7 @@ const Navbar = () => {
               <img alt="logo" src={logo}/>
             </Box>
             <Box sx={{ flexGrow: 1, textAlign: "right" }}>
-              {[ "about us", "our services", "case studies", "contact us" ].map(page => (
+              {navPages.map(page => (
                 <Button key={page} color="secondary" size="small"
                         sx={{ mr: { md: "1rem", lg: "2rem" }, fontSize: "0.8rem" }}>{page}</Button>
               ))}
